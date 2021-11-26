b88714c7ff9c4a6f36e0c7b3418e06470799a124
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2021-11-26 19:31:58 +0100

    added code for task 4

---
+++ Task_4/mahalanobis_distance.m
@@ -0,0 +1,24 @@
+function mahalanobis_distance()
+    mu = [3, 4];
+    sigma = [1, 0; 0, 2];
+    
+    points = [10, 10; 0, 0; 3, 4; 6, 8];
+    size_points = size(points);
+    num_points = size_points(1);
+    
+    for i=1:num_points
+        dist = compute_mahalanobis_distance(points(i, :), mu, sigma);
+        fprintf("mahalanobis dist of (%d, %d) from (%d, %d) is %.3f\n", points(i, 1), points(i, 2), mu(1), mu(2), dist);
+    end
+end
+
+function maha_dist = compute_mahalanobis_distance(x, mu, sigma)
+    rank_sigma = rank(sigma);
+    if rank_sigma < min(size(sigma))
+        fprintf("entered covariance matrix is singular\n");
+        maha_dist = 0;
+    else
+        sigma_inv = inv(sigma);
+        maha_dist = sqrt((x - mu) * sigma_inv * (x - mu)');
+    end
+end
---
+++ Task_4/multi_variate_gaussian.m
@@ -0,0 +1,18 @@
+function multi_variate_gaussian()
+    mu = [3, 4];
+    sigma = [1, 0; 0, 2];
+    
+    generate_multi_variate_gaussian(mu, sigma);
+end
+
+function generate_multi_variate_gaussian(mu, sigma)
+    x = linspace(-10, 10);
+    y = linspace(-10, 10);
+    [xx, yy] = meshgrid(x, y);
+    z = mvnpdf([xx(:) yy(:)], mu, sigma);
+    z = reshape(z, size(xx));
+    mesh(xx, yy, z);
+    title("2D Gaussian PDF");
+    xlabel("x_1");
+    ylabel("x_2");
+end
