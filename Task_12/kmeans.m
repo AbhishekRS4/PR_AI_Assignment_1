761852efb9c09cae1b69941a28c9ae18c1d92fd2
Bartelsh <40543177+Bartelsh@users.noreply.github.com>
2021-12-14 14:41:28 +0100

    implemented 12-2

--- Task_12/kmeans.m
+++ Task_12/kmeans.m
@@ -1,30 +1,57 @@
 clear all;
 load kmeans1.mat;
+load checkerboard.mat;
 
-K=4;
+% settings
+K=2;
 data = kmeans1;
-
-% determine range
-[maxval ~] = max(abs(data));
-
-% initialize K means randomly
-means = zeros(K, 2);
-for i=1:K
-    means(i, 1) = (rand - 0.5) * 2 * maxval(1);
-    means(i, 2) = (rand - 0.5) * 2 * maxval(2);
+% data = checkerboard;
+Kmeansplusplus = false;
+if K > 8
+    plotFlag = false;
+else
+    plotFlag = true;
 end
 
+
+% initialization
+
+% determine range
+[maxval, ~] = max(abs(data));
+if Kmeansplusplus == false % initialize K means randomly
+    means = zeros(K, 2);
+    for i=1:K
+        means(i, 1) = (rand - 0.5) * 2 * maxval(1);
+        means(i, 2) = (rand - 0.5) * 2 * maxval(2);
+    end
+else % initialize according to Kmeans++
+    randomIndex = round(length(data) * rand);
+    means = zeros(1, 2);
+    means(1, :) = data(randomIndex, :);
+    
+    for j=2:K
+        distances = zeros(length(data), 1);
+        for i=1:length(data) 
+            [distances(i), ~] = nearestNeighbor(data(i,:), means, length(means(:, 1)));
+            distances(i) = distances(i)^2 * rand;
+        end
+        [distance, index] = max(distances);
+        means(j, :) = data(index, :);
+    end
+end
+    
+    
 % run loop
 labels = zeros(length(data), 1);
 change = true;
 
-counter = 0
+counter = 0;
 while change
     counter = counter + 1;
-    historicalMeans(:, :, counter) = means
+    historicalMeans(:, :, counter) = means;
     oldLabels = labels;
     for i=1:length(data)
-        labels(i) = nearestNeighbor(data(i, :), means, K);
+        [~, labels(i)] = nearestNeighbor(data(i, :), means, K);
     end
     if oldLabels == labels
         change = false;
@@ -33,42 +60,53 @@ while change
 end
 
 
+% quantizationError = calcQuantizationError(data, means)
 
-
-% plot results
-tiledlayout(1, 2);
-colors = {'o', '+', '*', 'd', 's', 'x', '.', 'v'};
-X = data(:, 1);
-Y = data(:, 2);
-nexttile;
-hold on;
-for i =1:K
-    plot(X(labels == i), Y(labels == i), colors{i});
+quantizationError = 0;
+for i=1:length(data)
+    quantizationError = quantizationError + sum((data(i, :)-means(labels(i), :)).^2);
 end
-plot(means(:, 1), means (:, 2), 'kp');
+quantizationError
 
-nexttile;
-hold on;
-for i=1:K
-    for j=1:(counter-1)
-        plot_arrow(historicalMeans(i,1,j), historicalMeans(i,2,j) ,historicalMeans(i,1,j+1), historicalMeans(i,2,j+1));
+
+if plotFlag == true
+    % plot results
+    t = tiledlayout(1, 2);
+    colors = {'o', '+', '*', 'd', 's', 'x', '.', 'v'};
+    X = data(:, 1);
+    Y = data(:, 2);
+    nexttile;
+    hold on;
+    for i =1:K
+        plot(X(labels == i), Y(labels == i), colors{i});
     end
+    plot(means(:, 1), means (:, 2), 'kp');
+
+    nexttile;
+    hold on;
+    for i=1:K
+        for j=1:(counter-1)
+            plot_arrow(historicalMeans(i,1,j), historicalMeans(i,2,j) ,historicalMeans(i,1,j+1), historicalMeans(i,2,j+1));
+        end
+    end
+    plot(means(:, 1), means (:, 2), 'kp');
+    % axis([-6 8 -4 3]);
+    % exportgraphics(t,'12-1-8.pdf')
 end
-plot(means(:, 1), means (:, 2), 'kp');
-axis([-6 8 -4 3])
+
 
 
 % return nearest neighbor class index
-function class = nearestNeighbor(coords, means, K)
+function [distance, class] = nearestNeighbor(coords, means, K)
     distance_table = (1:K);
     for i = 1:K
         distance_table(i) = sqrt(sum((means(i, :)-coords).^2));
     end
     sorted_labels = sortrows(distance_table);
-    [~, class] = min(distance_table);
+    [distance, class] = min(distance_table);
 end
     
-% recalculate the means
+% recalculate the K means
 function newMeans = update(data, labels, means, K)
     newMeans = zeros(K, 2);
     n = zeros(K);
@@ -81,7 +119,6 @@ function newMeans = update(data, labels, means, K)
         newMeans(i, :) = newMeans(i, :) / n(i);
     end
 end
-
 
 
 
